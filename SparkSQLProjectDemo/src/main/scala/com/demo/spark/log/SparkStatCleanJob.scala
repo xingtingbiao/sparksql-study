18e1850789254d34c8f3fd26ba236a2588008c8f
xingtingbiao <xingtingbiao@znlhzl.com>
2018-11-06 11:59:05 +0800

    feature: 清洗完的数据写到parquet文件

--- SparkSQLProjectDemo/src/main/scala/com/demo/spark/log/SparkStatCleanJob.scala
+++ SparkSQLProjectDemo/src/main/scala/com/demo/spark/log/SparkStatCleanJob.scala
@@ -1,7 +1,7 @@
 package com.demo.spark.log
 
 import com.demo.spark.utils.AccessConvertUtil
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 /**
   * 第二步: 使用Spark完成我们的数据清洗工作
@@ -15,8 +15,12 @@ object SparkStatCleanJob {
     // rdd ==> DF
     val row = rdd.map(line => AccessConvertUtil.parseLog(line))
     val frame = spark.createDataFrame(row, AccessConvertUtil.struct)
-    frame.printSchema()
-    frame.show()
+//    frame.printSchema()
+//    frame.show()
+
+    // coalesce()用于将小文件合并的, partitionBy(colNames: String*)根据字段分区的
+    frame.coalesce(1).write.format("parquet").mode(SaveMode.Overwrite)
+      .partitionBy("day").save("/home/xingtb/lib/clean")
 
     spark.stop()
   }
